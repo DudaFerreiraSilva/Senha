47c68b24393898d9ee840b5fee207764ac6f5031
Eduarda Ferreira <edududaferreira10@gmail.com>
2022-11-01 19:32:13 -0300

    Resolução

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿const string SenhaCorreta = "1234abcd";
+string senha, permissaoAcesso;
+
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.Write("Olá, usuário. Por favor, digite sua senha: ");
+senha = Console.ReadLine()!;
+Console.ResetColor();
+
+permissaoAcesso = senha == SenhaCorreta ? "permitido" : "negado";
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine($"Acesso {permissaoAcesso}");
+Console.ResetColor();
